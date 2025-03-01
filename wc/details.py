c3cf1f3d827174e8d48bcdb17796f087b643e8e1
Xorgack <xorgack@proton.me>
2025-03-01 20:31:52 +0330

    Add byte count

--- wc/details.py
+++ wc/details.py
@@ -1,4 +1,4 @@
-from os.path import exists, isfile
+from os.path import exists, isfile, getsize
 from sys import stdin
 
 def get_file_details(file_path):
@@ -7,6 +7,7 @@ def get_file_details(file_path):
         "is_file": True,
         "lines": 0,
         "words": 0,
+        "bytes": 0,
     }
 
     # Check path validity
@@ -17,6 +18,10 @@ def get_file_details(file_path):
         details["is_file"] = False
         return details
     
+    # Get size
+    details["bytes"] += getsize(file_path)
+
+
     with open(file_path, "r") as data:
         for line in data:
             in_word = False
@@ -34,19 +39,24 @@ def get_file_details(file_path):
 def get_stdin_details():
     details = {
         "lines": 0,
-        "words":0
+        "words":0,
+        "bytes": 0,
     }
 
-    data = stdin.read()
+    line = stdin.readline()
+    while line:
+        details["bytes"] += len(line)
 
-    in_word = False
-    for c in data:
-        if not c.isspace():
-            in_word = True
-        elif in_word:
-            in_word = False
-            details["words"] += 1
-        if c == "\n":
-            details["lines"] += 1
-    
+        in_word = False
+        for c in line:
+            if not c.isspace():
+                in_word = True
+            elif in_word:
+                in_word = False
+                details["words"] += 1
+            if c == "\n":
+                details["lines"] += 1
+
+        line = stdin.readline()
+
     return details
--- wc/report.py
+++ wc/report.py
@@ -3,6 +3,7 @@ from enum import Enum
 class State(Enum):
     LINES = 1
     WORDS = 2
+    BYTES = 4
 
 def get_state(args):
     state = 0
@@ -10,12 +11,20 @@ def get_state(args):
         state |= State.LINES.value
     if args.words:
         state |= State.WORDS.value
-    return state
+    if args.bytes:
+        state |= State.BYTES.value
+
+    # Return default if no flag provided
+    return state or (State.LINES.value | State.WORDS.value | State.BYTES.value)
 
 def report(details, state, title):
     print("", end=" ")
+
     if state & State.LINES.value:
         print(f"{details['lines']:3}", end="\t")
     if state & State.WORDS.value:
         print(f"{details['words']:3}", end="\t")
+    if state & State.BYTES.value:
+        print(f"{details['bytes']:3}", end="\t")
+
     print(title)
